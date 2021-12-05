717620a96eb778d47ed7858d483cdf7b8f3f66bc
Chris <chunt661@mtroyal.ca>
2021-12-05 14:56:06 -0700

    added details tab

---
+++ src/components/Details.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Layout, Tabs, Row, Col, List } from 'antd';
+
+import './Details.css';
+
+const { Content } = Layout;
+const { TabPane } = Tabs;
+
+const Details = (props) => {
+    const [ play, setPlay ] = useState([]);
+    const { id } = useParams();
+    
+    // Load play data
+    useEffect( () => {
+        const getData = async () => {
+            console.log('fetching');
+            let data = JSON.parse(localStorage.getItem(id));
+            if (!data) {
+                try {
+                    const url = 'http://localhost:8081/api/play/' + id;
+                    fetch(url)
+                        .then(response => response.json())
+                        .then(data => {
+                            setPlay(data[0]);
+                            localStorage.setItem(id, JSON.stringify(data[0]))
+                        });
+                } catch (err) {
+                    console.error(err);
+                }
+            } else {
+                console.log(data);
+                setPlay(data);
+            }
+        };
+        
+        getData();
+    }, [id]);
+    
+    return (
+        <Content id='details'>
+            <h1>{play.title}</h1>
+            <Tabs defaultActiveKey='1'>
+                <TabPane tab='Details' key='1'>
+                    <Row gutter={32}>
+                        <Col span={18}>{play.desc}</Col>
+                        <Col span={6}>
+                            <h3>Details</h3>
+                            <dl>
+                                <dt>Year:</dt>
+                                <dd>{play.likelyDate}</dd>
+                                <dt>Genre:</dt>
+                                <dd>{play.genre}</dd>
+                            </dl>
+                            <h3>Links</h3>
+                            <ul>
+                                <li><a href={play.wiki}>Wikipedia</a></li>
+                                <li><a href={play.gutenberg}>Project Gutenberg</a></li>
+                                <li><a href={play.shakespeareOrg}>Shakespeare.org</a></li>
+                            </ul>
+                        </Col>
+                    </Row>
+                </TabPane>
+                <TabPane
+                    tab='Characters'
+                    key='2'
+                    {...(!play.filename && {disabled: true})}>
+                </TabPane>
+                <TabPane
+                    tab='Text'
+                    key='3'
+                    {...(!play.filename && {disabled: true})}>
+                    
+                </TabPane>
+            </Tabs>
+        </Content>
+    );
+};
+
+export default Details;
