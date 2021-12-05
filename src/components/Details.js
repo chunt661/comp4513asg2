b965a3a9c823193a09cea365500a97e6b3819e3e
Chris <chunt661@mtroyal.ca>
2021-12-05 15:55:17 -0700

    added play text tab

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,20 +1,21 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Layout, Tabs, Row, Col, List } from 'antd';
+import { Layout, Tabs, Row, Col, List, Select } from 'antd';
 
 import './Details.css';
 
 const { Content } = Layout;
 const { TabPane } = Tabs;
+const { Option } = Select;
 
 const Details = (props) => {
     const [ play, setPlay ] = useState([]);
+    
     const { id } = useParams();
     
     // Load play data
     useEffect( () => {
         const getData = async () => {
-            console.log('fetching');
             let data = JSON.parse(localStorage.getItem(id));
             if (!data) {
                 try {
@@ -29,7 +30,6 @@ const Details = (props) => {
                     console.error(err);
                 }
             } else {
-                console.log(data);
                 setPlay(data);
             }
         };
@@ -65,26 +65,105 @@ const Details = (props) => {
                     tab='Characters'
                     key='2'
                     {...(!play.filename && {disabled: true})}>
-                    <List
-                        itemLayout='horizontal'
-                        dataSource={play.playText.persona}
-                        renderItem={player => (
-                            <List.Item>
-                                <List.Item.Meta
-                                    title={player.player}
-                                    description={player.desc} />
-                            </List.Item>
-                        )}
-                        />
+                    {
+                        play.playText &&
+                            <List
+                                itemLayout='horizontal'
+                                dataSource={play.playText.persona}
+                                renderItem={player => (
+                                    <List.Item>
+                                        <List.Item.Meta
+                                            title={player.player}
+                                            description={player.desc} />
+                                    </List.Item>
+                                )}
+                                />
+                    }
                 </TabPane>
                 <TabPane
                     tab='Text'
                     key='3'
                     {...(!play.filename && {disabled: true})}>
-                    
+                    { play.playText && <PlayText title={play.playText.title} acts={play.playText.acts} /> }
                 </TabPane>
             </Tabs>
         </Content>
+    );
+};
+
+const PlayText = (props) => {
+    const [ currentAct, setAct ] = useState(props.acts[0]);
+    const [ currentScene, setScene ] = useState(props.acts[0].scenes[0]);
+    const [ currentPlayer, setPlayer ] = useState(null);
+    
+    // Generate list of players
+    const players = [];
+    currentScene.speeches.forEach(s => {
+        if (!players.includes(s.speaker)) {
+            players.push(s.speaker);
+        }
+    });
+    
+    const handleActChange = (value) => {
+        const newAct = props.acts.find(a => a.name == value);
+        setAct(newAct);
+        setScene(newAct.scenes[0]); // Set scene to first scene of the act
+        setPlayer(null); // Set player to all players
+    };
+    
+    const handleSceneChange = (value) => {
+        const newScene = currentAct.scenes.find(s => s.name == value);
+        setScene(newScene);
+        setPlayer(null); // Set player to all players
+    };
+    
+    const handlePlayerChange = (value) => {
+        setPlayer(value);
+    }
+    
+    return (
+        <Row gutter={32}>
+            <Col span={8}>
+                <h3>Play Navigation</h3>
+                <Select value={currentAct.name} onChange={handleActChange}>
+                    { props.acts.map(a => (
+                        <Option value={a.name} key={a._id}>{a.name}</Option>
+                    )) }
+                </Select>
+                <Select value={currentScene.name} onChange={handleSceneChange}>
+                    { currentAct.scenes.map(s => (
+                        <Option value={s.name} key={s._id}>{s.name}</Option>
+                    )) }
+                </Select>
+                <Select value={currentPlayer} onChange={handlePlayerChange}>
+                    <Option value={null}>ALL PLAYERS</Option>
+                    { players.map(p => (
+                        <Option value={p} key={p}>{p}</Option>
+                    )) }
+                </Select>
+            </Col>
+            <Col span={16}>
+                <section>
+                <h2>{props.title}</h2>
+                    <article>
+                        <h3>{currentAct.name}</h3>
+                        <div>
+                            <h4>{currentScene.name}</h4>
+                            <p className='title'>{currentScene.title}</p>
+                            <p className='direction'>{currentScene.stageDirection}</p>
+                            { currentScene.speeches.map(s => (
+                                <div key={s._id} className='speech'>
+                                    <span className='speaker'>{s.speaker}</span>
+                                    { s.lines.map((line, i) => (
+                                        <p key={i}>{line}</p>
+                                    ))}
+                                </div>
+                            )) }
+                        </div>
+                    </article>
+                </section>
+            </Col>
+        </Row>
     );
 };
 
