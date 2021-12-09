fa4bea56107aa44def2912f11a2e9865aa1d46cb
Chris <chunt661@mtroyal.ca>
2021-12-09 10:50:41 -0700

    Revert "comment"
    This reverts commit c156663007499422d06a57e3a4394a67fae51e54.

--- server.js
+++ server.js
@@ -19,16 +19,17 @@ const router = require('./scripts/apiRouter.js');
 require('./scripts/dbConnector.js').connect();
 
 
-/* --- MIDDLEWARE --- */
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+
+/* --- MIDDLEWARE --- */
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.use(session({
-    secret: process.env.secret,
+    secret: process.env.SECRET,
     resave: true,
     saveUninitialized: true
 }));
@@ -73,17 +74,10 @@ app.get('/logout', (req, res) => {
     res.render('login', {message: req.flash('info')});
 });
 
-// Helper route to retrieve the ID of the currently logged-in user. This is to
-// allow user info to be fetched on the client side
-app.get('/auth', (req, res) => {
-    const id = req.user ? req.user.id : null;
-    res.json({ userID: id });
-});
-
 
 app.use((req, res, next) => { res.status(404).send('Bad request.') });
 
-const port = process.env.api_port;
+const port = process.env.port;
 app.listen(port, () => {
     console.log('listening on port ' + port);
 });
